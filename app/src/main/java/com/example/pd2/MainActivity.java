57f4f6afdea2da1ee1cc18a22aed5ee7b5faabb6
robertssumanis <sumanis4321@gmail.com>
2021-12-14 14:55:31 +0200

    hello world

---
+++ app/src/main/java/com/example/pd2/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.pd2;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateAlertDialog();
+            }
+        });
+
+        Button on_Second = (Button) findViewById(R.id.on_second);
+        on_Second.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(), "GOING TO SECOND ACTIVITY", Toast.LENGTH_SHORT).show();
+                Intent tosecond = new Intent (getApplicationContext(), SecondActivity.class);
+                startActivity(tosecond);
+            }
+        });
+    }
+    private void CreateAlertDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("8 Group's Dialog");
+
+        builder.setMultiChoiceItems(R.array.Names, null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                String arr[] = getResources().getStringArray(R.array.Names);
+
+                if (isChecked) {
+                    Toast.makeText(getApplicationContext(), arr[which] + " IS CHECKED", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), arr[which] + " IS UNCHECKED", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        Toast.makeText(getApplicationContext(), "CANCEL IS CLICKED", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        builder.setPositiveButton(android.R.string.ok, null);
+
+        builder.create();
+        //builder.show();
+
+        Button positiveButton = builder.show().getButton(AlertDialog.BUTTON_POSITIVE);
+        positiveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                Toast.makeText(getApplicationContext(), "OK IS CLICKED", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/pd2/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.example.pd2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button back = (Button) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(), "GOING BACK", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+}
